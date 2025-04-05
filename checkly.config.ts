79c3ec130c9630be8ea902cf8572dce5bac46eac
Alex Suzuki <alex@pixelverse.ch>
2025-04-05 10:18:56 +0200

    build: added checkly config

--- checkly.config.ts
+++ checkly.config.ts
@@ -1,28 +1,15 @@
-import { defineConfig } from 'checkly'
-import { Frequency } from 'checkly/constructs'
+import {defineConfig} from 'checkly'
+import {Frequency} from 'checkly/constructs'
 
 export default defineConfig({
- projectName: 'Website Monitoring',
- logicalId: 'website-monitoring-1',
- repoUrl: 'https://github.com/acme/website',
- checks: {
-   activated: true,
-   muted: false,
-   runtimeId: '2022.10',
-   frequency: Frequency.EVERY_5M,
-   locations: ['us-east-1', 'eu-west-1'],
-   tags: ['website', 'api'],
-   checkMatch: '**/*.check.js',
-   browserChecks: {
-     frequency: Frequency.EVERY_10M,
-     testMatch: 'browsers/**/*.spec.js',
-   },
-   multiStepChecks: {
-     testMatch: 'multistep/**/*.spec.js',
-   },
- },
- cli: {
-   runLocation: 'eu-west-1',
-   privateRunLocation: 'private-dc1'
- }
+    projectName: 'strich-web-demo-test',
+    logicalId: 'trich-web-demo-test',
+    repoUrl: 'https://github.com/suzukieng/strich-web-demo-test',
+    checks: {
+        playwrightConfigPath: './playwright.config.ts',
+    },
+    cli: {
+        runLocation: 'eu-west-1',
+        retries: 0
+    }
 })
