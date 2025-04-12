e11a308fbe97ad3903751ef9a6b62cb68811d387
Alex Suzuki <alex@pixelverse.ch>
2025-04-12 10:50:49 +0800

    Revert "feat: record trace for every test run"
    This reverts commit 285860b458d4a78c9acf9c79731367d284c088d2.

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     workers: process.env.CI ? 1 : undefined,
     reporter: 'html',
     use: {
-        trace: 'on',
+        trace: 'on-first-retry',
         video: 'on'
     },
     projects: [
