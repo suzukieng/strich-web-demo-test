f413f3e3d5f1991f4bc91b4eba4847a23df32fea
Alex Suzuki <alex@pixelverse.ch>
2025-09-19 09:17:44 +0200

    test: restore video include section

--- checkly.config.ts
+++ checkly.config.ts
@@ -2,29 +2,31 @@ import { defineConfig } from 'checkly'
 import { Frequency } from 'checkly/constructs';
 
 const config = defineConfig({
-  logicalId: 'strich-web-demo-test',
-  projectName: 'strich-web-demo-test',
-  checks: {
-    playwrightConfigPath: './playwright.config.ts',
-    playwrightChecks: [
-      {
-        logicalId: 'strich-web-demo-test',
-        name: 'Basic Scanning Test',
-        testCommand: 'npx playwright test ',
-        locations: [
-          'eu-central-1',
+    logicalId: 'strich-web-demo-test',
+    projectName: 'strich-web-demo-test',
+    checks: {
+        playwrightConfigPath: './playwright.config.ts',
+        playwrightChecks: [
+            {
+                logicalId: 'strich-web-demo-test',
+                name: 'Basic Scanning Test',
+                testCommand: 'npx playwright test ',
+                locations: [
+                    'eu-central-1',
+                ],
+                frequency: Frequency.EVERY_24H, // a custom per-check frequency
+            },
         ],
-        frequency: Frequency.EVERY_24H, // a custom per-check frequency
-      },
-    ],
-    frequency: Frequency.EVERY_24H, // a global default frequency
-    locations: [
-      'eu-central-1', // a global default location
-    ],
-  },
-  cli: {
-    runLocation: 'eu-central-1', // where test and pw-test will run
-  },
+        frequency: Frequency.EVERY_24H, // a global default frequency
+        locations: [
+            'eu-central-1', // a global default location
+        ],
+        // include test-videos to test barcode scanning from camera
+        include: '**/test-videos/*'
+    },
+    cli: {
+        runLocation: 'eu-central-1', // where test and pw-test will run
+    },
 })
 
 export default config
