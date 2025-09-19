abbc5e6c768976819a974127f21b568365f102fb
Alex Suzuki <alex@pixelverse.ch>
2025-09-19 09:24:00 +0200

    tests: add slight delay at the end to get usage recorded

--- tests/scan-oned.spec.ts
+++ tests/scan-oned.spec.ts
@@ -1,32 +1,38 @@
-import {test, expect, chromium} from '@playwright/test';
+import { test, expect, chromium } from '@playwright/test';
 
 test('Scans EAN/UPC Book Barcode', async () => {
-  const browser = await chromium.launch({
-    args: [
-      "--use-fake-ui-for-media-stream",
-      "--use-fake-device-for-media-stream",
-      "--use-file-for-fake-video-capture=./test-videos/book_barcode.mjpeg",
-    ]
-  });
-  const context = await browser.newContext({
-    permissions: ['camera'],
-    recordVideo: {
-      dir: 'videos/'
-    }
-  });
-  const page = await context.newPage();
+    const browser = await chromium.launch({
+        args: [
+            "--use-fake-ui-for-media-stream",
+            "--use-fake-device-for-media-stream",
+            "--use-file-for-fake-video-capture=./test-videos/book_barcode.mjpeg",
+        ]
+    });
+    const context = await browser.newContext({
+        permissions: ['camera'],
+        recordVideo: {
+            dir: 'videos/'
+        }
+    });
+    const page = await context.newPage();
 
-  // check start page is displayed
-  await page.goto('https://demo.strich.io/');
-  await expect(page).toHaveTitle(/JavaScript Barcode Scanner Demo/);
+    // check start page is displayed
+    await page.goto('https://demo.strich.io/');
+    await expect(page).toHaveTitle(/JavaScript Barcode Scanner Demo/);
 
-  // select 1D retail barcodes and start scanning
-  await page.getByText("1D Retail").click();
-  await page.getByText("START SCANNING").click();
+    // select 1D retail barcodes and start scanning
+    await page.getByText("1D Retail").click();
+    await page.getByText("START SCANNING").click();
 
-  // fake video stream starts, barcode should be read
-  await expect(page.getByText('9781292223049')).toBeVisible();
+    // fake video stream starts, barcode should be read
+    await expect(page.getByText('9781292223049')).toBeVisible();
 
-  // finalize video
-  await context.close();
+    // finish scanning will cause the beacon request to be scheduled
+    await page.getByText('FINISH SCANNING').click();
+
+    // add a little delay for the Beacon request to be sent
+    await page.waitForTimeout(1000);
+
+    // finalize video
+    await context.close();
 });
